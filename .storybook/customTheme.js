0049595f230e08bbaebb341c3b434ced4dfc52fa
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-06-26 19:03:25 +0200

    add light/dark mode storybook

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -1,21 +1,31 @@
 import { create } from "@storybook/theming";
+import logo from "./static/onyxiaLogo.png";
 
-export default create({
+export const darkTheme = create({
+    "base": "dark",
+    "brandTitle": "Disiz UI",
+    "appBg": "#2c323f",
+    "appContentBg": "#2c323f",
+    "barBg": "#2c323f",
+    "colorSecondary": "#ff562c",
+    "textColor": "#f1f0eb",
+    "textInverseColor": "#2c323f",
+    "brandImage": logo,
+    "brandUrl": "https://github.com/InseeFrLab/disiz",
+
+    "fontBase": '"Open Sans", sans-serif',
+    "fontCode": "monospace",
+});
+
+export const lightTheme = create({
     "base": "light",
     "brandTitle": "Disiz UI",
+    "appBg": "#f1f0eb",
+    "appContentBg": "#f1f0eb",
+    "barBg": "#f1f0eb",
+    "colorSecondary": "#ff562c",
+    "textColor": "#2c323f",
+    "textInverseColor": "#f1f0eb",
+    "brandImage": logo,
     "brandUrl": "https://github.com/InseeFrLab/disiz",
-    "brandImage": "/onyxiaLogo.png", //TODO ADD disiz logo
-
-    // Toolbar default and active colors
-    //"barTextColor": 'pink',
-    //"barSelectedColor": 'black',
-
-    "colorPrimary": "#2C323F",
-    "colorSecondary": "#2C323F",
-    "inputBg": "pink",
-    "inputBorder": "pink",
-    "a": "pink",
-
-    "fontBase": '"Work Sans","Open Sans", sans-serif',
-    "fontCode": "monospace",
 });
